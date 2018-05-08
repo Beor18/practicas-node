9690c2fdd0f2a07d3a299c4740f6ed63b510dedc
Fernando Lopez <fer.ragnar19@gmail.com>
2018-05-07 22:40:22 -0300

    agregamos comandos
    Signed-off-by: Fernando Lopez <fer.ragnar19@gmail.com>

---
+++ config/yargs.js
@@ -0,0 +1,18 @@
+const opts = {
+    base: {
+        demand: true,
+        alias: 'b'
+    },
+    limite: {
+        alias: 'l',
+        default: 10
+    }
+}
+
+
+// Comandos
+
+
+module.exports = {
+    argv
+}
