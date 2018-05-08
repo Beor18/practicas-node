dee38a0e21ddfcd3deab44081c7f7b0003aee258
Fernando Lopez <fer.ragnar19@gmail.com>
2018-05-07 22:41:02 -0300

    modificado
    Signed-off-by: Fernando Lopez <fer.ragnar19@gmail.com>

--- config/yargs.js
+++ config/yargs.js
@@ -12,6 +12,11 @@ const opts = {
 
 // Comandos
 
+const argv = require('yargs')
+    .command('listar', 'Imprime en consola la tabla de multiplicar', opts)
+    .command('crear', 'Genera un archivo con la tabla de multiplicar', opts)
+    .help()
+    .argv;
 
 module.exports = {
     argv
