245081d9b91526cde2da8c1e9685daa7605c8f58
Fernando Lopez <fer.ragnar19@gmail.com>
2018-05-09 11:02:51 -0300

    se agrega token con jwt
    Signed-off-by: Fernando Lopez <fer.ragnar19@gmail.com>

--- rest-server/server/routes/login.js
+++ rest-server/server/routes/login.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 
 const Usuario = require('../models/usuario');
 const app = express();
@@ -35,11 +36,16 @@ app.post('/login', (req, res) => {
                 }
             });
         }
+        // Se agrega token
+
+        let token = jwt.sign({
+            usuario: usuarioDB
+        }, 'seed-desarrollo', { expiresIn: process.env.CADUCIDAD_TOKEN });
 
         res.json({
             ok: true,
             usuario: usuarioDB,
-            token: '123'
+            token
         });
     });
 
