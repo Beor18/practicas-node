b4acf10472dcbf3f6d0d9295dca220840a82de49
Fernando Lopez <fer.ragnar19@gmail.com>
2018-05-09 08:52:33 -0300

    paginacion de usuarios
    Signed-off-by: Fernando Lopez <fer.ragnar19@gmail.com>

--- rest-server/server/routes/usuario.js
+++ rest-server/server/routes/usuario.js
@@ -13,7 +13,36 @@ app.get('/', function(req, res) {
 });
 
 app.get('/usuarios', function(req, res) {
-    res.json('Get usuarios');
+
+    // Paginacion de usuarios 
+
+    let desde = req.query.desde || 0;
+
+    desde = Number(desde);
+
+    let limite = req.query.limite || 5;
+    limite = Number(limite);
+
+    Usuario.find({})
+        .skip(desde)
+        .limit(limite)
+        .exec((err, usuarios) => {
+            if (err) {
+                return res.status(400).json({
+                    ok: false,
+                    err
+                });
+
+            }
+
+            res.json({
+                ok: true,
+                usuarios
+            });
+        })
+
+    // Fin paginacion de usuarios
+
 });
 
 app.post('/usuarios', function(req, res) {
