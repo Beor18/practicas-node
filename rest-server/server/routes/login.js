e189f922f89741afa977f8263c5edef40835d5ae
Fernando Lopez <fer.ragnar19@gmail.com>
2018-05-09 10:49:26 -0300

    creamos login
    Signed-off-by: Fernando Lopez <fer.ragnar19@gmail.com>

--- rest-server/server/routes/login.js
+++ rest-server/server/routes/login.js
@@ -7,9 +7,43 @@ const app = express();
 
 app.post('/login', (req, res) => {
 
-    res.json({
-        ok: true,
+    let body = req.body;
+
+    Usuario.findOne({ email: body.email }, (err, usuarioDB) => {
+        if (err) {
+            return res.status(500).json({
+                ok: false,
+                err
+            });
+
+        }
+
+        if (!usuarioDB) {
+            return res.status(400).json({
+                ok: false,
+                err: {
+                    mensaje: 'Usuario o contraseña incorrectos'
+                }
+            });
+        }
+
+        if (!bcrypt.compareSync(body.password, usuarioDB.password)) {
+            return res.status(400).json({
+                ok: false,
+                err: {
+                    mensaje: 'Usuario o contraseña incorrectos'
+                }
+            });
+        }
+
+        res.json({
+            ok: true,
+            usuario: usuarioDB,
+            token: '123'
+        });
     });
+
+
 });
 
 
