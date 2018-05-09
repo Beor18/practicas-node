ccb8c3f5fb5fce75d5316a08d7bcabe053c38a5a
Fernando Lopez <fer.ragnar19@gmail.com>
2018-05-09 11:02:39 -0300

    dependencia jwt y cambios en config
    Signed-off-by: Fernando Lopez <fer.ragnar19@gmail.com>

--- rest-server/server/config/config.js
+++ rest-server/server/config/config.js
@@ -6,6 +6,25 @@ process.env.PORT = process.env.PORT || 3000;
 
 process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
 
+// Vencimiento Token
+// 60 segundos
+// 60 minutos
+// 24 horas
+// 30 dias
+
+process.env.CADUCIDAD_TOKEN = 60 * 60 * 24 * 30;
+
+// Semilla de autentificacion
+
+process.env.SEMILLA = process.env.SEMILLA || 'seed-desarrollo';
+
+///////////////////////////////////////////////////////////////
+// Para heroku agregar variable de entorno llamado SEMILLA  ///
+//
+// heroku config:set SEMILLA="seed-desarrollo"    /////////////
+//
+///////////////////////////////////////////////////////////////
+
 // Base de datos
 
 let urlDB;
