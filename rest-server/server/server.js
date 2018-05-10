95165ce608c4d28c2717f452c5b53bdb7860f1a5
Fernando Lopez <fer.ragnar19@gmail.com>
2018-05-09 23:09:50 -0300

    cambios
    Signed-off-by: Fernando Lopez <fer.ragnar19@gmail.com>

--- rest-server/server/server.js
+++ rest-server/server/server.js
@@ -3,6 +3,7 @@ require('./config/config');
 
 const express = require('express');
 const mongoose = require('mongoose');
+const path = require('path');
 
 const app = express();
 
@@ -13,6 +14,8 @@ app.use(bodyParser.urlencoded({ extended: false }))
 
 // parse application/json
 app.use(bodyParser.json())
+
+app.use(express.static(path.resolve(__dirname, '../public')));
 
 // Configuracion global de rutas
 app.use(require('./routes/index'));
