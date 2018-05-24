d7ac44bf2eb5221ac75be90ede053ea2f10cbce8
Fernando Lopez <fer.ragnar19@gmail.com>
2018-05-23 23:22:39 -0300

    cambios
    Signed-off-by: Fernando Lopez <fer.ragnar19@gmail.com>

--- login-sencillo/routes/index.js
+++ login-sencillo/routes/index.js
@@ -3,6 +3,8 @@ const passport = require('passport');
 const router = express.Router();
 const path = require('path');
 
+const Productos = require('../models/producto');
+
 const fileUpload = require('express-fileupload');
 router.use(fileUpload());
 
@@ -17,6 +19,74 @@ router.get('/', (req, res, next) => {
 router.get('/perfil', isLoggedIn, (req, res) => {
     res.render('perfil.ejs', { user: req.user });
 });
+
+// Un nuevo CRUD //
+
+router.get('/nuevo', isLoggedIn, (req, res) => {
+    Productos.find(function(err, person) {
+        if (err) return next(err);
+        res.render('nuevo.ejs', {
+            user: req.user,
+            person
+        });
+    });
+});
+
+router.get('/listar-producto', isLoggedIn, (req, res, next) => {
+    Productos.find(function(err, products) {
+        if (err) return next(err);
+        res.json(products)
+
+    });
+    //res.render('nuevo.ejs', { user: req.user, producto: req.productos });
+});
+
+router.post('/nuevo', function(req, res) {
+    let body = req.body
+
+    let producto = new Productos({
+        titulo: body.titulo,
+        autor: body.autor,
+        descripcion: body.descripcion
+    })
+
+    producto.save(() => {
+        res.redirect('/nuevo');
+    });
+});
+
+// EDITAR PRODUCTO FALTA TERMINAR
+router.get('/editar/:id', isLoggedIn, (req, res, next) => {
+    //let o_id = req.params.id;
+    //let titulo = req.params.titulo;
+
+    Productos.findById(req.params.id, function(err, person) {
+        if (err) return next(err);
+        res.render('editar.ejs', {
+            user: req.user,
+            person
+        });
+    });
+
+});
+
+router.put('/update', (req, res) => {
+
+    Productos.findOneAndUpdate(req.params.id, { titulo: req.body.titulo }, function(err, person) {
+
+        if (err) {
+            console.log(err);
+            res.redirect("/perfil");
+        } else {
+            res.redirect('/editar/' + _id);
+        }
+
+    });
+});
+
+// FIN EDITAR PRODUCTO FALTA TERMINAR
+
+// FIN UN NUEVO CRUD //
 
 // FIN solo para el usuario
 
