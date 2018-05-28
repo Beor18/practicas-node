c3044d937fd18324901d41dc517678196e857aaa
Fernando Lopez <fer.ragnar19@gmail.com>
2018-05-28 10:49:14 -0300

    manejo de sockets
    Signed-off-by: Fernando Lopez <fer.ragnar19@gmail.com>

---
+++ login-sencillo/socketEvento.js
@@ -0,0 +1,25 @@
+const { io } = require('./app');
+const Productos = require('./models/producto');
+
+var usuarioContador = 0;
+
+io.on('connection', (client) => {
+
+    usuarioContador++;
+    io.emit('users.count', usuarioContador);
+
+    var count = 1;
+    var stream = Productos.find().stream();
+
+    stream.on('data', function(productos) {
+        io.sockets.emit('comment.count', {
+            count: count++
+        });
+    });
+
+    client.on('disconnect', function() {
+        usuarioContador--;
+        io.emit('users.count', usuarioContador);
+    });
+
+});
