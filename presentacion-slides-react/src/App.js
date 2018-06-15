bed31ff46cd9d1f7747a3e3cd046c1781f46e710
Beor18 <fer.ragnar19@gmail.com>
2018-06-14 21:33:58 -0300

    creo los slides

--- presentacion-slides-react/src/App.js
+++ presentacion-slides-react/src/App.js
@@ -1,21 +1,38 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import { Impress, Step } from 'react-impressjs';
+
+//Estilo de react-impress
+import 'react-impressjs/styles/react-impressjs.css';
 import './App.css';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
-}
+
+const defaultDesplacement = 1000;
+
+const App = () => (
+  <Impress
+    progress={true}
+    fallbackMessage={<p>Errorrr</p>}
+  >
+  <Step className="slide" id="impress">
+   <div className="text-center"> 
+     <h3>Presentación sobre TensorFlow JS</h3>
+     <p className="autor"><strong>Por Fernando López 2018</strong></p>
+   </div>
+  </Step>
+
+  <Step className="slide" data={{ x: defaultDesplacement}}>
+  <div className="text-center"> 
+     <h3>¿Que es TensorFlow?</h3>
+   </div>
+  </Step>
+
+  <Step className="step" id="bored" data={{ x: -1000, y: -1500 }}>
+  <div className="text-center-dos"> 
+     <p>* Es una Biblioteca OpenSource</p>
+     <p>* Redes Neuronales de aprendizaje profundo</p>
+   </div>
+  </Step>
+  </Impress>
+);
 
 export default App;
